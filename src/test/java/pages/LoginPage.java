9c2837b10d6379e2e8d505ee7afd02621a5a3b0c
Shpilevskaya Natasha <sh-natasha1989@yandex.ru>
2022-06-08 19:57:46 +0300

    HGJHG

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+    SelenideElement username= $(By.id("name"));
+
+}
